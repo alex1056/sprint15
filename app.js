4875b194c0d20592d41721bd118412add1a79765
alexander.b <gituser1056@gmail.com>
2020-08-05 10:49:27 +0300

    Выполнена централизованная обработка ошибок для карточек

--- app.js
+++ app.js
@@ -35,6 +35,21 @@ app.use(auth);
 app.use('/', routerUsers);
 app.use('/', routerCards);
 app.use(routerErr);
+// app.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,39 @@
 const jwt = require('jsonwebtoken');
+const AuthRequiredError = require('../errors/auth-required-err');
 
 module.exports.auth = (req, res, next) => {
-  if (!req.cookies.jwt) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+  let token;
+  try {
+    token = req.cookies.jwt;
+  } catch (e) {
+    const err = new AuthRequiredError('Необходима авторизация');
+    err.statusCode = 401;
+    next(err);
   }
-  const token = req.cookies.jwt;
+
+  //  if (!req.cookies.jwt) {
+  //    return res
+  //      .status(401)
+  //      .send({ message: 'Необходима авторизация' });
+  //   const err = new AuthRequiredError('Необходима авторизация');
+  //   err.statusCode = 401;
+  //   next(err);
+  // }
+  // const token = req.cookies.jwt;
 
   let payload;
 
   try {
     const { NODE_ENV, JWT_SECRET } = process.env;
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
-  } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+  } catch (e) {
+    // return res
+    //   .status(401)
+    //   .send({ message: 'Необходима авторизация' });
+    const err = new AuthRequiredError('Необходима авторизация');
+    // console.log(err.message);
+    err.statusCode = 401;
+    next(err);
   }
 
   req.user = payload;
