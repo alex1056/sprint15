c4080f7ae9a275c5499517d11fe7bf3e5f7209ca
alexander.b <gituser1056@gmail.com>
2020-08-05 17:09:01 +0300

    Реализовано логирование запросов и ошибок

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const routerCards = require(path.join(__dirname, 'routes/router-cards.js'));
 const routerSignInUp = require(path.join(__dirname, 'routes/router-signin-signup.js'));
 
 const routerErr = require(path.join(__dirname, 'middlewares/router-err.js'));
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -29,11 +30,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(requestLogger);
 app.use('/', routerSignInUp);
 app.use(auth);
 app.use('/', routerUsers);
 app.use('/', routerCards);
 app.use(routerErr);
+app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
